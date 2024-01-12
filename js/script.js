1f4509045eaa5840248566cd11ccd32c0c09bd00
Haydar Çoban <hydr.cbn@gmail.com>
2024-01-12 11:30:34 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//  typing animation
+
+
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Web Designer","Software Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
